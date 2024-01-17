3371a61b95a8467bb1879f4ce7c5386042e6453e
Ethanic <emirmfathan@gmail.com>
2024-01-17 15:53:58 +0700

    test

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import About from "./About.jsx";
+import About from "./about/about.jsx";
 import Error from "./error.jsx";
 
 const router = createBrowserRouter([
