fb8962499c90f0070a5b2fac37b270fa5ba8a210
Faris Zhafir Faza <111985990+Marsupilamieue@users.noreply.github.com>
2024-01-18 20:50:33 +0700

    update tech page

---
+++ src/technology/components/Arrow.jsx
@@ -0,0 +1,9 @@
+const Arrow = () => {
+  return (
+    <div className="w-24 overflow-hidden inline-block my-24">
+        <div className=" h-16 w-16 bg-[#5EBB47] -rotate-45 transform origin-top-left"></div>
+    </div>
+  )
+}
+
+export default Arrow
---
+++ src/technology/components/ArrowR.jsx
@@ -0,0 +1,9 @@
+const ArrowR = () => {
+  return (
+    <div className="w-3  overflow-hidden inline-block">
+        <div className=" h-4  bg-black rotate-45 transform origin-top-left"></div>
+    </div>
+  )
+}
+
+export default ArrowR
