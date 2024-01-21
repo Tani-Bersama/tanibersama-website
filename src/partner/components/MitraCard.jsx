dfaa9db052be5eaf1a317c5d3dfce460f5810389
Ethanic <emirmfathan@gmail.com>
2024-01-21 10:54:08 +0700

    partner done

--- src/partner/components/MitraCard.jsx
+++ src/partner/components/MitraCard.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 export default function MitraCard({ title, children }) {
   return (
-    <div className="flex flex-col items-center m-10">
-      <div>{title}</div>
-      <div className="flex flex-wrap justify-between">{children}</div>
+    <div className="flex flex-col items-center m-10 font-all justify-center">
+      <div className="font-semibold">{title}</div>
+      <div
+        className="flex flex-wrap justify-center max-w-[500px] gap-4
+                    text-center border-2 border-[#55A840] rounded-xl p-4 m-5"
+      >
+        {children}
+      </div>
     </div>
   );
 }
